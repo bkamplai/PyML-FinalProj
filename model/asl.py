5d7908f119cff94f543394aef9e33066b6497891
elenasx <eeschmitt@mavs.coloradomesa.edu>
2025-02-17 10:51:53 -0700

    added code to asl.py

--- model/asl.py
+++ model/asl.py
@@ -21,11 +21,13 @@ Authors: Brandon Kamplain, Aidan Kavanagh, Elena Schmitt
 """
 
 # Enter values for mean and std dev depending on model chosen
-model_mean = []
-model_std = []
+# mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] (resnet values)
+model_mean = [0.485, 0.456, 0.406]
+model_std = [0.229, 0.224, 0.225]
 
 # Transform for images to keep consistent
 transform = transforms.Compose([
+    transforms.ToPILImage(), # Convert image to PIL to be able to use ToTensor()
     transforms.Resize(512, 512), # All images in the dataset are 512x512
     transforms.ToTensor(), # Convert the image to a tensor for torch
     transforms.Normalize(mean=model_mean, std=model_std) # Mean and std dev for model
@@ -36,14 +38,25 @@ transform = transforms.Compose([
 Dataset class for ASL alphabet images dataset.
 """
 class ASLDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, file, directory, transform=None):
+        self.data = pd.read_csv(file) # Use pandas to read csv
+        self.directory = directory # Directory of image
+        self.transform = transform # Apply transform to image
+        self.letter = self.data.columns[1:].tolist() # Get letter image represents from csv
     
     def __len__(self):
-        pass
+        return len(self.data) # Return length of data
 
-    def __getitem__(self):
-        pass
+    def __getitem__(self, idx):
+        image_name = os.path.join(self.directory, self.data.iloc[idx, 0]) # Gets filename of image
+        image = cv2.imread(image_name)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Convert image from BGR to RGB
+        label = torch.tensor(self.data.iloc[idx, 1:].values.astype(np.float32)) # Get labels from csv and conver to float
+
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
 
 
 """
@@ -60,6 +73,9 @@ def test():
     pass
 
 
+"""
+Main function where train and test will be called.
+"""
 def main() -> None:
     pass
 
