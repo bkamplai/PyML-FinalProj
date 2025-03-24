12b8303c5fd892be0b139977c661af80185bd6d6
elenasx <89545479+elenasx@users.noreply.github.com>
2025-03-24 10:38:59 -0600

    added image upload

--- app/app.py
+++ app/app.py
@@ -12,16 +12,40 @@ app = Flask(__name__)
 # Load the model
 model = tf.keras.models.load_model('../training/Models/asl_fingerspell_mobilenet_finetuned.keras')
 # All categories for the ASL alphabet
-labels = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") + ['Blank']
+labels = ['A', 'B', 'Blank', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 # For uploading video
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'mp4', 'mov', 'avi'}
 
-# Default video path
+# Default image/video path
+uploaded_image_path = None
 uploaded_video_path = None
 
 # Handle uploads
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    global uploaded_image_path
+
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file'})
+    image = request.files['image']
+
+    if image.filename == '':
+        return jsonify({'error': 'No file selected'})
+    
+    if image:
+        filename = secure_filename(image.filename)
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image.save(image_path)
+
+        uploaded_image_path = image_path
+
+        # Image sucessfully uploaded
+        return jsonify({'message': "Image uploaded"})
+    else:
+        return jsonify({'error': 'Incorrect file format'})
+
 @app.route('/upload_video', methods=['POST'])
 def upload_video():
     global uploaded_video_path
@@ -53,6 +77,39 @@ def video_stream():
         return jsonify({'error': 'No video uploaded'})
     return Response(predict_letters(uploaded_video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Run image through model
+@app.route('/predict_image', methods=['GET'])
+def predict_image():
+    global uploaded_image_path
+    if uploaded_image_path is None:
+        return jsonify({'error': 'No image uploaded'})
+
+    # Prepare the image
+    image = cv2.imread(uploaded_image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (128, 128))
+    image_resized = image_resized / 255.0
+    image_resized = np.expand_dims(image_resized, axis=0)
+
+    # Predict letter in image
+    prediction = model.predict(image_resized)
+    letter_prediction = np.argmax(prediction)
+    letter = labels[letter_prediction]
+    confidence = prediction[0][letter_prediction]
+
+    cv2.putText(image, f"Predicted signed letter: {letter}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, f"Confidence: {prediction[0][letter_prediction] * 100:.2f}%", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert back to RGB for proper display in browser
+    _, jpeg = cv2.imencode('.jpg', image_rgb)
+
+    if jpeg is not None:
+        # Convert image to byte stream for transmission
+        image_bytes = jpeg.tobytes()
+        return Response(image_bytes, content_type='image/jpeg')
+    else:
+        return jsonify({'error': 'Failed to process image'})
+
 # Function to take in video and run frames through the model
 def predict_letters(video_path):
     # Start the webcam (uploading video for now)
@@ -82,8 +139,8 @@ def predict_letters(video_path):
         # predictions.append(letter)
 
         # Display letter prediction on top left
-        cv2.putText(frame, f"Predicted signed letter: {letter}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.putText(frame, f"Confidence: {prediction[0][letter_prediction]}", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(frame, f"Predicted signed letter: {letter}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, f"Confidence: {prediction[0][letter_prediction] * 100:.2f}%", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
         # Convert frame to JPEG and use as response
         _, jpeg = cv2.imencode('.jpg', frame)
         if jpeg is not None:
